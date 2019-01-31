85451db467c277212e6a9349451bfed67c9e115d
suman <suman.kunwar@daliaresearch.com>
2019-01-31 18:13:06 +0100

    added enviroment variables for test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+console.log(process.env.REACT_APP_BASE_URL, 'BASE_URL==>')
 class App extends Component {
   render() {
     return (
